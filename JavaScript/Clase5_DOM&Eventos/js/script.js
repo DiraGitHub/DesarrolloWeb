bc224253938e3b0f404a9e4e27b5fe01c5321caf
Erendira Hernandez <erendira.hernandez@tpvsolutions.com.mx>
2022-03-29 17:53:02 -0600

    Actualización de nombres de carpetas

---
+++ JavaScript/Clase5_DOM&Eventos/js/script.js
@@ -0,0 +1,54 @@
+console.log(document.body);
+
+
+let nodo = document;
+console.log(nodo);
+
+nodo = document.all;
+console.log(nodo);
+console.log(nodo[6]);
+
+
+nodo = document.all[2];
+console.log(nodo);
+
+nodo = document.head;
+console.log(nodo);
+
+nodo = document.body;
+console.log(nodo);
+
+nodo = document.scripts;
+console.log(nodo);
+
+nodo = document.scripts[0];
+console.log(nodo);
+
+nodo = document.domain;
+console.log(nodo);
+
+nodo = document.forms;
+console.log(nodo);
+
+nodo = document.URL;
+console.log(nodo);
+
+nodo = document.characterSet;
+console.log(nodo);
+
+nodo = document.links;
+console.log(nodo);
+
+nodo = document.images;
+console.log(nodo);
+
+//SE PUEDE USAR UN CONSOLE.DIR PARA QUE EL ELEMENTO SE DESPLIEGUE EN FORMA DE OBJETO
+console.dir(nodo);
+
+nodo = document.images[0];
+
+
+
+
+
+
